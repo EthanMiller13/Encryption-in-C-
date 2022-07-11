119adbfb914c17b2fc000a4dd68a194269dd6f75
Grizolda <79770415+Grizolda@users.noreply.github.com>
2022-07-11 10:15:30 +0300

    message

--- Program.cs
+++ Program.cs
@@ -6,9 +6,10 @@ namespace MainAssembly{
             Encryption.EncryptionManager manager = new Encryption.EncryptionManager(rand.Next(1, 100));
             string my_text = "Hi there! I'm Ethan and I've created this program!";
             string enc = manager.encrypt(my_text, manager.seed);
+            Console.WriteLine("Encrypted: " + enc, "Seed: " + manager.seed);
+
             string dec = manager.decrypt(enc, manager.seed);
-            Console.WriteLine("Encrypted: " + enc);
-            Console.WriteLine("Decrypted: " + dec);
+            Console.WriteLine("Decrypted: " + dec, "Seed: " + manager.seed);
         }
     }
 }
